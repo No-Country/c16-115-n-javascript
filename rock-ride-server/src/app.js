387409bf71ec21dc9ec9fb154499dcd34c677d57
tatoclemente <tatoapuros@gmail.com>
2024-02-09 21:27:52 -0300

    First commit server

---
+++ rock-ride-server/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+// const path = require('path')
+// const Routes = require('./routes')
+
+const server = express()
+
+server.use(cors())
+
+server.use(logger('dev'))
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+
+server.use(express.json())
+  
+// server.use(Routes)
+
+// server.use(express.static(path.join(__dirname, 'public')))
+
+module.exports = server
