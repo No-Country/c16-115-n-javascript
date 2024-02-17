c49642b5c99002cf9479d5d7882c18e8bd125c6f
jfpanchi <jfpanchi2@espe.edu.ec>
2024-02-17 13:22:42 -0500

    feat: :sparkles: Add eventRouter
    Add eventRouter for routes in events

--- rock-ride-server/src/routes/index.js
+++ rock-ride-server/src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import authRouter from './auth.js'
+import eventRouter from './events.js'
 
 const router = Router()
 
 
 router.use('/auth', authRouter)
-
+router.use('/events',eventRouter)
 
 export default router
