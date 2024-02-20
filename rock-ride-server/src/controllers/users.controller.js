604da9ff172abe109b2fda7017beafea6c5ae13c
tatoclemente <tatoapuros@gmail.com>
2024-02-19 22:45:02 -0300

    User Route in process. Get, Put, And  Login

---
+++ rock-ride-server/src/controllers/users.controller.js
@@ -0,0 +1,159 @@
+
+import { Op } from "sequelize";
+import { User } from "../database.js";
+import { validate as validateUuid } from "uuid";
+
+
+
+export const getUserById = async (id) => {
+  if (!validateUuid(id)) {
+    return res
+      .status(400)
+      .json({ ok: false, message: "Invalid eventId format" });
+  }
+
+
+  try {
+
+    const user = await User.findByPk(id);
+    return {
+      ok: true,
+      user
+    }
+  } catch (error) {
+    console.log(error.message);
+    return {
+      ok: false,
+      message: 'Error fetching user',
+    }
+  }
+}
+
+
+
+export const getUsers = async (name) => {
+
+  try {
+
+    if (name) {
+      const users = await User.findAll({
+        where: {
+          fullName: {
+            [ Op.iLike ]: `%${ name.toLowerCase() }%` 
+          }
+        }
+      })
+      if (!users.length) {
+        return {
+          ok: false,
+          message: "User not found"
+        } 
+      } else {
+        console.log(users)
+        return {
+          ok: true,
+          users,
+        }
+      }
+    }
+
+    const users = await User.findAll();
+    return { 
+      ok: true, 
+      users 
+    };
+  } catch (error) {
+    console.log(error.message);
+    return { 
+      ok: false, 
+      message: "Internal server error" 
+    };
+  }
+}
+
+export const updateUser = async (id, userRole, { fullName, email, password, isDriver, plate, address, city, role, active, profileImg, carPhotos }) => {
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return {
+        ok: false,
+        message: "User not found"
+      }
+    }
+
+    
+    if (userRole === 'user') {
+
+      if (role !== undefined || active !== undefined) {
+        return {
+          ok: false,
+          message: "Permission denied: Regular users cannot update role or active status."
+        };
+      }
+
+      const [rowsUpdated, [updatedUser]] = await User.update(
+        {
+          fullName,
+          email,
+          password,
+          isDriver,
+          plate,
+          address,
+          city,
+          profileImg,
+          carPhotos
+        },
+        {
+          where: { id },
+          returning: true,
+        }
+        
+      );
+
+      if (rowsUpdated > 0) {
+        return {
+          ok: true,
+          event: updatedUser,
+        };
+      } else {
+        return {
+          ok: false,
+          message: "User not found",
+        };
+      }
+
+    }
+
+    if (userRole === 'admin') {
+      const [rowsUpdated, [updatedUser]] = await User.update(
+        {
+          role,
+          active,
+        },
+        {
+          where: { id },
+          returning: true,
+        }
+      )
+
+      if (rowsUpdated > 0) {
+        return {
+          ok: true,
+          event: updatedUser,
+          message: "User successfully updtaded"
+        };
+      } else {
+        return {
+          ok: false,
+          message: "User not found",
+        };
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    return {
+      ok: false,
+      message: "Internal server error"
+    }
+  }
+}
--- rock-ride-server/src/handlers/auth.handler.js
+++ rock-ride-server/src/handlers/auth.handler.js
@@ -1,10 +1,10 @@
-import { createNewUser } from "../controllers/registrer.controller.js";
+import { createNewUser, login } from "../controllers/auth.controller.js";
 
 
 export const registerHandler = async (req, res) => {
   const { email, password, address, city, fullName, phone, isDriver, plate } = req.body;
-  
-  if ( !email || !password || !fullName || !phone || !address || !city ) {
+
+  if (!email || !password || !fullName || !phone || !address || !city) {
     return res.status(400).json({ ok: false, message: "Email and password are required" });
   }
 
@@ -12,10 +12,26 @@ export const registerHandler = async (req, res) => {
   try {
 
     const { ok, token, message } = await createNewUser(email, password, address, city, fullName, phone, isDriver, plate)
-    
+
     res.status(201).json({ ok, token, message })
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ ok, message: "Internal server error" });
+  }
+}
+
+
+
+
+export const loginHandler = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+
+    const { ok, token, message } = await login(email, password)
+    res.status(200).json({ ok, message, token });
+    
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 }
