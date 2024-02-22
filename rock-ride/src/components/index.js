98bdbdd69b92ef1aa21f7282099aa5a38d664432
Bach16 <gwyn56717@gmail.com>
2024-02-22 14:52:39 -0500

    Update index.js

--- rock-ride/src/components/index.js
+++ rock-ride/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as RegisterButton } from './Buttons/RegisterButton';
+export { default as RegisterInput } from './Inputs/RegisterInput';
 export { default as NavBar } from './NavBar/NavBar';
+export { default as Divider } from './Ui/Divider';
