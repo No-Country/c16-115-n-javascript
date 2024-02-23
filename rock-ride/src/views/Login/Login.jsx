0639b95cd49b383b24307960b8216221d05542ed
Bach16 <gwyn56717@gmail.com>
2024-02-23 15:34:44 -0500

    Update Login.jsx

--- rock-ride/src/views/Login/Login.jsx
+++ rock-ride/src/views/Login/Login.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import { NavBar,  RegisterInput } from "../../components";
+import { LoginForm } from "./ui/LoginForm";
+import logo from '../../assets/imgs/drive-rock-v1.webp'
 
-const Login = () => {
+
+export default function LoginPage() {
   return (
-    <div>
-      <NavBar />
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52 items-center gap-6 bg-slate-900">
 
-      <div>
-        <div>
-            <RegisterInput text={"Registrate con email"}/>
-            <RegisterInput text={"Registrate con Google"}/>
-        </div>
-
-      <h2>Login</h2>
+      <div className="flex flex-col items-center gap-2 text-blue-400 font-['monserrat']">
+        <img width={100} height={100} src={ logo } alt="logo" />
+        <h2 className="text-xl font-serif italic">Drive Rock</h2>
       </div>
+      <div className="flex flex-col gap-4 items-center">
+      <h1 className='text-4xl text-slate-200'>Ingresar</h1>
 
+      <LoginForm />
+      </div>
     </div>
   );
-};
-
-export default Login;
+}
