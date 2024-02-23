b6ab9bac376d87726769082b19b9eec7aace01e4
Bach16 <gwyn56717@gmail.com>
2024-02-23 15:34:40 -0500

    Create index.js

---
+++ rock-ride/src/views/index.js
@@ -0,0 +1,7 @@
+export { default as Auth } from './Auth/Auth';
+export { default as EventPage } from './Event/EventPage';
+export { default as HomePage } from './Home';
+export { default as LoginPage } from './login/Login';
+export * from './login/ui/LoginForm';
+export { default as RegisterPage } from './register/Register';
+export * from './register/ui/RegisterForm';
