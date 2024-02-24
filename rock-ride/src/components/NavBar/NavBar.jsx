59cfcd4f95094b0fbea65a7fb9a8c0a420a5b02e
Bach16 <gwyn56717@gmail.com>
2024-02-24 15:20:15 -0500

    Update NavBar.jsx

--- rock-ride/src/components/NavBar/NavBar.jsx
+++ rock-ride/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { NavLink, useNavigate } from "react-router-dom";
+import logo from '../../../public/drive-rock-simple-v2.ico'
+
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -8,11 +10,17 @@ const NavBar = () => {
     <div className="flex w-[100%] items-center justify-center">
       <div className=" flex items-center justify-between w-[90%]">
         <div className="flex w-[40%] items-center justify-between">
-          <div className=" cursor-pointer flex items-center justify-between w-[8rem] h-[5rem]">
-            <img
-              className="h-[2rem]"
-              src="https://lanman2018.ieee-lanman.org/files/2016/01/sample-logo@2x.png"
-            />
+          <div className="flex items-center justify-between  text-[#222222] font-['monserrat']">
+            {/* <img
+              width={50}
+              height={50}
+              src={logo}
+              alt="logo"
+              className="drop-shadow-2xl-light"
+            /> */}
+            <h2 className="text-4xl  font-['Barbaro'] tracking-wide">
+              Drive Rock
+            </h2>
           </div>
           <div className=" flex items-center h-[5rem]">
             <ul className="flex w-[15rem] justify-between">
@@ -22,7 +30,7 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
-        <div className="flex justify-between w-[20rem]">
+        <div className="flex justify-between w-[17rem]">
           <NavLink to={"/auth/sign-in"}>
             <button className="btn-secondary  ">Iniciar sesion</button>
           </NavLink>
