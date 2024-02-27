473c9d6e8d67c8e99a3cdb6462a0112bbe0d067b
Bach16 <gwyn56717@gmail.com>
2024-02-26 19:15:19 -0500

    Create InfoItem.jsx

---
+++ rock-ride/src/components/Ui/InfoItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const InfoItem = ({icon,title,content}) => {
+    return (
+        <div className={` flex flex-col w-[25%] justify-between text-center h-[14rem] items-center`}>
+          <div className="  flex flex-col items-center justify-between  ">
+            <div className="text-[4rem]">
+            {icon}
+            </div>
+            <p className="text-[1.8rem] w-[100%] leading-auto font-bold">
+              {title}
+            </p>
+            <p className="text-[0.9rem] leading-[0.9rem] w-[90%] sm:w-[22rem] font-regular  ">
+              {content}
+            </p>
+          </div>
+        </div>
+      );
+}
+
+export default InfoItem
