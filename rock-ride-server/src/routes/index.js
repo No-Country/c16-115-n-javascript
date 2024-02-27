dec98fa7a71d5c7ba32487e6a06d095e33df366a
jfpanchi <jfpanchi2@espe.edu.ec>
2024-02-26 20:42:15 -0500

    fix: :lipstick: Format code
    Formating code
    
    Co-Authored-By: Gustavo Clemente <52931287+tatoclemente@users.noreply.github.com>

--- rock-ride-server/src/routes/index.js
+++ rock-ride-server/src/routes/index.js
@@ -1,20 +1,16 @@
-import { Router } from 'express'
-import authRouter from './auth.js'
-import eventRouter from './events.js'
-import userRouter from './users.js'
-import tripRouter from './trips.js'
-import ticketRouter from './ticket.js'
+import { Router } from "express";
+import authRouter from "./auth.js";
+import eventRouter from "./events.js";
+import userRouter from "./users.js";
+import tripRouter from "./trips.js";
+import ticketRouter from "./ticket.js";
 
+const router = Router();
 
-const router = Router()
+router.use("/auth", authRouter);
+router.use("/users", userRouter);
+router.use("/events", eventRouter);
+router.use("/trips", tripRouter);
+router.use("/tickets", ticketRouter);
 
-
-router.use('/auth', authRouter)
-router.use('/users', userRouter)
-router.use('/events', eventRouter)
-router.use('/trips', tripRouter)
-router.use('/tickets', ticketRouter)
-
-
-export default router
-
+export default router;
