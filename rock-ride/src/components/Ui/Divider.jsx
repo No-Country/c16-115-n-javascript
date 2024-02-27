cc20490f7d4c7987420e9feb4b801b28f5abc0f6
Bach16 <gwyn56717@gmail.com>
2024-02-26 19:15:16 -0500

    Create Divider.jsx

---
+++ rock-ride/src/components/Ui/Divider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Divider = () => {
+  return (
+    <div className='w-[100%]  border-solid border-[1px] border-t-[#D2D2D2] h-[1px]'>
+
+    </div>
+  )
+}
+
+export default Divider
