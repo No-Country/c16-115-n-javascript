2877a2f49d9b69a80ae2a068da0f5e097bf290d3
jfpanchi <jfpanchi2@espe.edu.ec>
2024-02-28 13:37:13 -0500

    feat: :sparkles: Add bookingRouter
    Add bookingRouter for routes

---
+++ rock-ride-server/src/routes/booking.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+
+const bookingRouter = Router();
+
+
+export default bookingRouter;
--- rock-ride-server/src/routes/index.js
+++ rock-ride-server/src/routes/index.js
@@ -4,6 +4,7 @@ import eventRouter from "./events.js";
 import userRouter from "./users.js";
 import tripRouter from "./trips.js";
 import ticketRouter from "./ticket.js";
+import bookingRouter from "./booking.js";
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use("/users", userRouter);
 router.use("/events", eventRouter);
 router.use("/trips", tripRouter);
 router.use("/tickets", ticketRouter);
+router.use("/bookings", bookingRouter);
 
 export default router;
