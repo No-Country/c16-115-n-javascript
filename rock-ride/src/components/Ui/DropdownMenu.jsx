8bb95e34f1ada413ca4650e3105a5911b050e2a0
Bach16 <gwyn56717@gmail.com>
2024-02-29 17:08:34 -0500

    Update DropdownMenu.jsx

--- rock-ride/src/components/Ui/DropdownMenu.jsx
+++ rock-ride/src/components/Ui/DropdownMenu.jsx
@@ -1,56 +1,65 @@
 import { IoMenuOutline } from "react-icons/io5";
 import { FaCircleUser } from "react-icons/fa6";
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-  } from '@chakra-ui/react'
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useRef } from "react";
+import { decodeToken } from "react-jwt";
+import Divider from "./Divider";
 
 const DrowpDownMenu = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
   return (
     <div className="flex cursor-pointer text-[#222222] ">
       <div className="flex items-center justify-end">
         <div
-          ref={btnRef} onClick={onOpen}
+          ref={btnRef}
+          onClick={onOpen}
           className="flex  w-[7rem] h-[3rem] items-center justify-between px-[0.8rem] rounded-3xl border-solid border-[1px] border-[#c5c5c5]"
         >
           <IoMenuOutline className="h-[2rem] w-[2rem]" />
           <FaCircleUser className="h-[2rem] w-[2rem]" />
         </div>
       </div>
-      
 
       <Drawer
         isOpen={isOpen}
-        placement='right'
+        placement="right"
         onClose={onClose}
         finalFocusRef={btnRef}
-      
       >
         <DrawerOverlay />
-        <DrawerContent className="p-[2rem]"  backgroundColor="#f2f2f2">
+        <DrawerContent className="p-[2rem]" backgroundColor="#f2f2f2">
           <div className="flex items-center justify-end">
-
-          <DrawerCloseButton />
+            <DrawerCloseButton />
           </div>
           <DrawerHeader>Create your account</DrawerHeader>
 
           <DrawerBody>
-            <p>asd</p>
-            <p>asd</p>
-            <p>asd</p>
+            <p>Perfil</p>
+            <p>Mis viajes</p>
+            <p>Soporte</p>
+
+            <button>Log out</button>
+
+            {decodeToken && decodeToken.role === "admin" && (
+              <div>
+                <Divider/>
+                <p>Usuarios</p>
+                <p>eventos</p>
+              </div>
+            )}
           </DrawerBody>
 
-          <DrawerFooter>
-          </DrawerFooter>
+          <DrawerFooter></DrawerFooter>
         </DrawerContent>
       </Drawer>
       {/* {isOpen && (
