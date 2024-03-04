f01358944700440b4a35a0015c7ba42cee414f7c
jfpanchi <jfpanchi2@espe.edu.ec>
2024-03-04 13:08:43 -0500

    fix: :bug: Update store
    Update store for pesistence

--- rock-ride/src/redux/store.js
+++ rock-ride/src/redux/store.js
@@ -1,15 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { eventSlice } from "./features/eventSlice"
-import { tripSlice } from "./features/tripSlice"
-import { authSlice } from "./features/authSlice"
-import { ticketSlice } from "./features/ticketSlice"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { eventSlice } from "./features/eventSlice";
+import { tripSlice } from "./features/tripSlice";
+import { authSlice } from "./features/authSlice";
+import { ticketSlice } from "./features/ticketSlice";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  event: eventSlice.reducer,
+  trip: tripSlice.reducer,
+  auth: authSlice.reducer,
+  ticket: ticketSlice.reducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        event: eventSlice.reducer,
-        trip: tripSlice.reducer,
-        auth: authSlice.reducer,
-        ticket: ticketSlice.reducer
-    }
-})
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
